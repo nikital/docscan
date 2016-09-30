c8d96437da7ae3fd6e1e5d3f28e4e75cd681d72d
Nikita Leshenko <nikita@leshenko.net>
2016-09-30 17:27:55 +0300

    Playing a bit

--- src/main.cpp
+++ src/main.cpp
@@ -11,21 +11,30 @@ public:
 class MyFrame: public wxFrame
 {
 public:
-    MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+    MyFrame(const wxString& title);
 };
 
 wxIMPLEMENT_APP(MyApp);
 
 bool MyApp::OnInit()
 {
-    auto frame = new MyFrame {"Hello World", wxDefaultPosition, wxSize{450, 340}};
+    auto frame = new MyFrame {"Hello World"};
     frame->Show (true);
     return true;
 }
 
-MyFrame::MyFrame (const wxString& title, const wxPoint& pos, const wxSize& size)
-    : wxFrame {NULL, wxID_ANY, title, pos, size}
+MyFrame::MyFrame (const wxString& title)
+    : wxFrame {NULL, wxID_ANY, title}
 {
     auto menu_bar = new wxMenuBar;
     SetMenuBar (menu_bar);
+
+    auto ok = new wxButton {this, wxID_OK};
+    auto cancel = new wxButton {this, wxID_CANCEL};
+
+    auto sizer = new wxBoxSizer {wxHORIZONTAL};
+    auto sizerFlags = wxSizerFlags(0).Border(wxALL, 10);
+    sizer->Add (ok, sizerFlags);
+    sizer->Add (cancel, sizerFlags);
+    SetSizerAndFit (sizer);
 }
